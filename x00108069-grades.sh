538bd1d3201d2358b0a3dbb264a54776974f4d9f
x00108069 <x00108069>
2014-10-14 16:59:11 +0100

    I found a bug on test five(Please note)

---
+++ x00108069-grades.sh
@@ -0,0 +1,59 @@
+#Test1
+#Inputs: 80 , 90
+
+#Expected Output: Pass with distinction
+
+#Actual Output: Pass with distinction
+echo Test1
+echo Expected output Pass with distinction
+java grades 80 90
+
+#Test 2
+#Inputs: 30 , 90
+
+#Expected Output: Component Fail
+
+#Actual Output: Component Fail
+echo test2
+echo Expected Outcome Component Fail
+java grades 30 90
+
+#Test 3
+#Inputs: 90 , 10
+
+#Expected Output: Component Fail
+
+#Actual Output: Component Fail
+echo test3
+echo Expected Outcome Component Fail
+java grades 90 10
+
+#Test4
+#Inputs: 80 , 10
+
+#Expected Output: Component Fail
+
+#Actual Output: Component Fail
+echo test4
+echo Expected Outcome Component Fail 
+java grades 80 10
+
+#Test5
+#Inputs: 40 , 70
+
+#Expected Output: Pass
+
+#Actual Output: Fail
+echo test5
+echo Expected Output Pass
+java grades 40 70
+
+#Test6
+#Inputs: 30 , 39
+
+#Expected Output: Component Fail
+
+#Actual Output: Component Fail
+echo test6
+echo Expected Output Component Fail
+java grades 30 39
