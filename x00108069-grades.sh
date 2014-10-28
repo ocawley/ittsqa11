4bf871728da682515e87ce9b3cd3bb3dbaa3e0ab
x00108069 <x00108069@gmail.com>
2014-10-28 16:54:37 +0000

    updated shell script

--- x00108069-grades.sh
+++ x00108069-grades.sh
@@ -1,59 +1,90 @@
+#!/bin/bash
+#This test a piece of software
+
 #Test1
 #Inputs: 80 , 90
-
 #Expected Output: Pass with distinction
+output=$(java grades_v2 80 90)
 
-#Actual Output: Pass with distinction
-echo Test1
-echo Expected output Pass with distinction
-java grades 80 90
+if [[ $output = "Pass with distinction" ]] 
+then
+	echo "Test Case 1 Passed"
+else
+	echo "Test Case 1 Failed"
+fi
+
+##################################################################################
 
 #Test 2
 #Inputs: 30 , 90
-
 #Expected Output: Component Fail
+output=$(java grades_v2 30 90)
 
-#Actual Output: Component Fail
-echo test2
-echo Expected Outcome Component Fail
-java grades 30 90
+if [[ $output = "Component Fail" ]] 
+then
+	echo "Test Case 2 Passed"
+else
+	echo "Test Case 2 Failed"
+fi
+
+##################################################################################
 
 #Test 3
 #Inputs: 90 , 10
-
 #Expected Output: Component Fail
+output=$(java grades_v2 90 10)
 
-#Actual Output: Component Fail
-echo test3
-echo Expected Outcome Component Fail
-java grades 90 10
+if [[ $output = "Component Fail" ]] 
+then
+	echo "Test Case 3 Passed"
+else
+	echo "Test Case 3 Failed"
+fi
+
+##################################################################################
+ 
 
 #Test4
 #Inputs: 80 , 10
-
 #Expected Output: Component Fail
+output=$(java grades_v2 80 10)
 
-#Actual Output: Component Fail
-echo test4
-echo Expected Outcome Component Fail 
-java grades 80 10
+if [[ $output = "Component Fail" ]] 
+then
+	echo "Test Case 4 Passed"
+else
+	echo "Test Case 4 Failed"
+fi
 
+##################################################################################
+
+ 
 #Test5
 #Inputs: 40 , 70
-
 #Expected Output: Pass
+output=$(java grades_v2 40 70)
 
-#Actual Output: Fail
-echo test5
-echo Expected Output Pass
-java grades 40 70
+if [[ $output = "Pass" ]] 
+then
+	echo "Test Case 5 Passed"
+else
+	echo "Test Case 5 Failed"
+fi
 
+##################################################################################
+
+ 
 #Test6
 #Inputs: 30 , 39
-
 #Expected Output: Component Fail
+output=$(java grades_v2 30 39)
 
-#Actual Output: Component Fail
-echo test6
-echo Expected Output Component Fail
-java grades 30 39
+if [[ $output = "Component Fail" ]] 
+then
+	echo "Test Case 6 Passed"
+else
+	echo "Test Case 6 Failed"
+fi
+
+##################################################################################
+ 
